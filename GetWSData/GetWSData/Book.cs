fb4cd3591b3c4f5364c0786f7f86e05d7022e30a
Tarcisio Gambin <gambin.br@gmail.com>
2017-10-22 00:43:40 -0200

    Primeiro release, testando get de dados via webservices (XML)

---
+++ GetWSData/GetWSData/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace GetWSData
+{
+    [XmlRoot("book")] // definindo o elemento root para leitura dos nodes abaixo
+    [XmlType("book")] // como o retorno do XML é 'book' (ao invés de 'Book', que é o mesmo nome da classe) 
+                      // e o serialize é case-sensitive, foi necessário definir o XMLType como 'book'
+    public class Book
+    {
+        
+        public string author { get; set; }
+        
+        public string title { get; set; }
+        
+        public string genre { get; set; }
+        
+        public double price { get; set; }
+
+        public DateTime pub_date { get; set; }
+        
+        public string review { get; set; }
+
+        public override string ToString()
+        {
+            // return base.ToString();
+            return "Autor: " + author
+                   + "\n Titulo: " + title
+                   + "\n Genero: " + genre
+                   + "\n Preço: " + price.ToString("F2", CultureInfo.InvariantCulture)
+                   + "\n Data Publicação: " + pub_date.ToString("dd/MM/yyyy")
+                   + "\n Review: " + review + "\n\n";
+
+        }
+    }   
+}
---
+++ GetWSData/GetWSData/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using GetWSData.SampleWS;
+using System.IO;
+using System.Net;
+using System.Security;
+using System.Security.Cryptography;
+
+namespace GetWSData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Iniciando objeto WS
+            SampleWebService webService = new SampleWebService();
+            XmlDocument xmlDoc = new XmlDocument();
+
+            // Carregando 'xmldoc' com retorno da chamada em WS XML
+            // Como as tags do XML de retorno utilizam prefixo, mas não possuem namespace em todos os nodes,
+            // foi necessário fazer um replace do atual prefixo utilizado
+            xmlDoc.LoadXml(webService.GetXmlData().OuterXml.Replace("x:", ""));
+            
+            // Salvando XML local para analise de estrutura. Apenas para testes e validação de output
+            // xmldoc.Save("C:\\temp\\xmldoc.xml");
+
+            //Iniciando processo de deserialização em um objeto do tipo 'book' no conjunto bList
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Book>), new XmlRootAttribute("books"));
+            StringReader stringReader = new StringReader(xmlDoc.OuterXml);
+            List<Book> bList = (List<Book>)serializer.Deserialize(stringReader);
+
+            // Impressao de retorno em tela
+            for (int i = 0; i < bList.Count; i++)
+            {
+                Console.WriteLine(bList[i]);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
